599673c6d5100b2f71a986afc0aef004649227ff
Nick Wise <nickcwise@gmail.com>
2017-07-12 12:24:20 -0400

    updated exercises

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -59,12 +59,105 @@ namespace Lists
             //    }
             //}
 
-            List<string> favoriteFoods = new List<string>() { "Steak", "Pizza", "BBQ", "Ramen", "Hot Sauce" };
+            //List<string> favoriteFoods = new List<string>() { "Steak", "Pizza", "BBQ", "Ramen", "Hot Sauce" };
 
-            if(favoriteFoods.Contains("Ramen"))
+            //if(favoriteFoods.Contains("Ramen"))
+            //{
+            //    Console.WriteLine("Ahh yes, Ramen is great");
+            //}
+
+            //create a list with the following numbers: 1,23,9,77,922,6,32,63,14,5
+            //use the contains method with the following values: 23,77,15
+            //remove these elements: 27,77,32, and 6
+            //use Contains() again on these values: 23,77,15
+
+            //List <int> numList = new List<int>() { 1, 23, 9, 77, 922, 6, 32, 63, 14, 5 };
+            //if (numList.Contains(23))
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //if(numList.Contains(23))
+            //{
+            //    numList.RemoveAt(1);
+            //}
+            //if(numList.Contains(77))
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //if (numList.Contains(77))
+            //{
+            //    numList.RemoveAt(3);
+            //}
+            //if(numList.Contains(15))
+            //{
+            //    Console.WriteLine("False");
+            //}
+            //if(numList.Contains(32))
+            //{
+            //    numList.RemoveAt(6);
+            //}
+            //if(numList.Contains(6))
+            //{
+            //    numList.RemoveAt(5);
+            //}
+
+            //Ask the user for a movie
+            //if the movie is not in the list add it
+            //inform the user that the movie has been added
+            //if the user enters quit the program should exit
+            //The user should be abkle to add as many movies as they want
+
+            List<string> movieNames = new List<string>() { "GUARDIANS OF THE GALAXY", "BABY DRIVER", "WONDER WOMAN", "OKJA" };
+            string enterAnother = "Y";
+            while (enterAnother == "Y")
             {
-                Console.WriteLine("Ahh yes, Ramen is great");
+
             }
+            Console.WriteLine("Please enter a movie you would like to watch: ");
+            string userMovie = Console.ReadLine();
+            string userMovieAny = userMovie.ToUpper();
+            if (userMovieAny == movieNames[0] || userMovieAny == movieNames[1] || userMovieAny == movieNames[2] || userMovieAny == movieNames[3])
+            {
+                Console.WriteLine("Enjoy your Movie!");
+                Environment.Exit(0);
+            }
+            if (userMovieAny == "QUIT")
+            {
+                Console.WriteLine("Sorry to see you go!");
+                Environment.Exit(0);
+            }
+            if (userMovieAny != movieNames[0] || userMovieAny != movieNames[1] || userMovieAny != movieNames[2] || userMovieAny != movieNames[3])
+            {
+                Console.WriteLine("This movie is unavailable, would you like to add it to watch later? Y/N ");
+                string userResponse = Console.ReadLine();
+                string userResponseAny = userResponse.ToUpper();
+                if (userResponseAny == "Y")
+                {
+                    movieNames.Add(userResponseAny);
+                    Console.WriteLine("Thank you, your movie will be available in 3-5 business days.");
+                    Console.WriteLine("Would you like to add another movie to your list? Y/N ");
+                    string userAnother = Console.ReadLine();
+                    string userAnotherAny = userAnother.ToUpper();
+                    if (userAnotherAny == "Y")
+                    {
+
+                    }
+                    
+                }
+                else if (userResponseAny == "N")
+                {
+                    Console.WriteLine("No worries! We're sorry your movie was unavailable!");
+                    Environment.Exit(0);
+                }
+                else if (userResponseAny == "QUIT")
+                {
+                    Console.WriteLine("Sorry to see you go!");
+                    Environment.Exit(0);
+                }
+                
+            }
+
+
 
 
 
