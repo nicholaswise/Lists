f9f299e0dd4c47620478d7fc3812a635753b7eed
Nick Wise <nickcwise@gmail.com>
2017-07-12 10:52:46 -0400

    updated exercises

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -10,6 +10,64 @@ namespace Lists
     {
         static void Main(string[] args)
         {
+            /*List<int> favNumbers = new List<int>();
+            favNumbers.Add(7);
+            favNumbers.Add(25);
+            favNumbers.Add(30);
+
+            //foreach (int number in favNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //Use the Count property to get the length of the list
+            Console.WriteLine(favNumbers.Count);
+            List<string> books = new List<string> { "Cat in the Hat", "Toad and Frog", "Green Eggs and Ham" };
+            Console.WriteLine(books[0]);
+            //Inserts at the current index moving the previous element down one
+            books.Insert(0, "Horton Hears a Who");
+            Console.WriteLine(books[0]);*/
+
+            //Create a list and add 5 animals using the .Add()
+            //Print each animal in the list
+
+            //List<string> animals = new List<string>();
+            //animals.Add("Lion");
+            //animals.Add("Tiger");
+            //animals.Add("Bear");
+            //animals.Add("Koala");
+            //animals.Add("Giraffe");
+            //foreach(string animal in animals)
+            //{
+            //    Console.WriteLine(animal);
+            //}
+
+            ////Create the following list: a bool list {true,false,false,true,false}
+            ////loop through each value
+            ////if the value is true print "better bring an umbrella"
+            ////if the value is false print "no rain today enjoy the sun"
+
+            //List<bool> boolList = new List<bool> { true, false, false, true, false };
+            //foreach (bool word in boolList)
+            //{
+            //    if (word == true)
+            //    {
+            //        Console.WriteLine("Better bring an umbrella!");
+            //    }
+            //    if (word == false)
+            //    {
+            //        Console.WriteLine("No rain today enjoy the sun!");
+            //    }
+            //}
+
+            List<string> favoriteFoods = new List<string>() { "Steak", "Pizza", "BBQ", "Ramen", "Hot Sauce" };
+
+            if(favoriteFoods.Contains("Ramen"))
+            {
+                Console.WriteLine("Ahh yes, Ramen is great");
+            }
+
+
+
         }
     }
 }
